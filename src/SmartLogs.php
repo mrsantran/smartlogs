0b98a7d56c14751c92043365f6d0551a800e87a2
San Trần <santran686@gmail.com>
2016-12-23 09:39:49 +0700

    Add Log Reverse

--- src/SmartLogs.php
+++ src/SmartLogs.php
@@ -71,8 +71,14 @@ class SmartLogs
         if (config("smartlogs.log_condition")) {
             $list_users = config('smartlogs.list_user_id');
             $list_ip = config('smartlogs.list_ip');
-            if (in_array($this->client_ip, $list_ip) || in_array($this->user_id, $list_users)) {
-                $write_log = true;
+            if (!config("smartlogs.log_reverse")) {
+                if (in_array($this->client_ip, $list_ip) || in_array($this->user_id, $list_users)) {
+                    $write_log = true;
+                }
+            } else {
+                if (!in_array($this->client_ip, $list_ip) && !in_array($this->user_id, $list_users)) {
+                    $write_log = true;
+                }
             }
         } else {
             $write_log = true;
